31b26698357a56f3103257ab715b2368fa6e59a4
Cole Moore <Cartisim@icloud.com>
2022-03-02 14:03:51 +0800

    Initial Commit

---
+++ CBAAssist/CBAAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  CBAAssistApp.swift
+//  CBAAssist
+//
+//  Created by Cole M on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CBAAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
